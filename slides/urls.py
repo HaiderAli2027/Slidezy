e76b4389c62084140dc43c41f94f42d17e704e3b
HaiderAli2027 <haiderali223208@gmail.com>
2026-03-03 22:36:51 +0500

    Generate Slides Function implemented!

--- slides/urls.py
+++ slides/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.slide_builder, name = 'slide_builder')
+    path('', views.slide_builder, name = 'slide_builder'),
+    path('api/generate_slides/', views.generate_slides, name='generate_slides')
+
 ]
