1c543b46939c5f74e395b88d40be7fa9a962e7d6
HaiderAli2027 <haiderali223208@gmail.com>
2026-03-10 22:53:07 +0500

    google-genai configure and slides content is being generated!

--- slides/views.py
+++ slides/views.py
@@ -1,10 +1,69 @@
+from calendar import c
+import os
+from cmd import PROMPT
+from dotenv import load_dotenv
+
+
 from django.shortcuts import render
 import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-# from google import genai
-# from google.genai import types
+from google import genai
+from google.genai import types
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# client = genai.Client()
+
+def generate_slide_content(topic):
+    PROMPT = f"""
+    you generate a slide titles for presentation.
+    Return exactly five slide titles for a beginner friendly talk about "{topic}".
+    Must return only valid JSON in this exact structure:
+    {{
+        "slides": [
+        {{"title": "..."}},
+        {{"title": "..."}},
+        {{"title": "..."}},
+        {{"title": "..."}},
+        {{"title": "..."}}
+
+        ]
+    }}
+
+"""
+    try:
+        response = client.models.generate_content(
+            model = "gemini-2.0-flash",
+            contents = [{"text": PROMPT}],
+            config=types.GenerateContentConfig(response_mime_type = "application/json"),
+        )
+        raw = ""
+        for part in response.parts:
+            if part.text:
+                raw += part.text.strip()
+        
+        print("Raw title generation response:", raw)
+
+        data = json.loads(raw)
+        titles = [s["title"] for s in data.get("slides", []) if "title" in s]
+
+        if len(titles) == 5:
+            return titles
+    
+    except Exception as e:
+        print("Error generating slide titles:", str(e))
+    
+    return [
+        f"Introduction to {topic}",
+        f"Core Concepts of {topic}",
+        f"How {topic} Works",
+        f"Use Cases of {topic}",
+        f"Future of {topic}"
+    ]
 
 # Create your views here.
 def slide_builder(request):
@@ -24,13 +83,10 @@ def generate_slides(request):
     if not topic:
         topic = "Random topic"
 
-    titles = [
-        f"Introduction to {topic}",
-        f"Core Concepts of {topic}",
-        f"How {topic} Works",
-        f"Use Cases of {topic}",
-        f"Future of {topic}"
-    ]
+    print(f"Topic from client: '{topic}'")
+
+    titles = generate_slide_content(topic)
+    print(f"Titles from Ai: {titles}")
 
     slides = []
 
